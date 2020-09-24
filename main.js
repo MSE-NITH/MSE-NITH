f8ff660b319c5aa67261b0569646138aa6a8d94e
DOMSE1 <70532404+DOMSE1@users.noreply.github.com>
2020-09-24 18:04:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $(".nav-icon").click(function(){
+    $(".full-nav").addClass("open");
+ });
+    
+$(".nav-close").click(function(){
+    $(".full-nav").removeClass("open");
+});
+$ (window).scroll(function(){
+        var sc = $(window).scrollTop();
+        if( sc>100){
+            $ (".nav").addClass("sticky");
+        }
+        else{
+            $ (".nav").removeClass("sticky");
+        }
+
+
+});
+$("#Major").magnificPopup({
+    delegate:'a',
+    type:'image',
+    gallery:{
+        enabled:true
+    }
+});
+});
